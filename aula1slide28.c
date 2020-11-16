7c2babff987ddd8824b2f13edf2bc7305f42bd51
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2020-11-15 22:44:04 -0300

    nao consigo usar void *pBuffer

---
+++ aula1slide28.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char nome[50];
+    int idade;
+    int cpf;
+}Pessoa;
+
+typedef struct{
+    Pessoa *ppl;
+    int indice;
+    int opc;
+}Controle;
+
+void menu (int o);
+void insere (Controle *c);
+
+int main()
+{
+    Controle *pBuffer;
+    pBuffer = (Controle *)malloc(sizeof(Controle));
+    pBuffer->opc =0;
+
+    while (pBuffer->opc != 5)
+    {
+        menu(pBuffer->opc);
+        switch (pBuffer->opc)
+        {
+            case 1:
+                insere(pBuffer);
+        }
+
+    }
+
+}
+
+void menu (int o)
+{
+    printf ("AULA 1 SLIDE 28 - EH O GUTHS\n");
+    printf ("1. Inserir pessoa\n");
+    printf ("2. Apagar pessoa\n");
+    printf ("3. Buscar pessoa\n");
+    printf ("4. Listar pessoas\n");
+    printf ("5. Sair do programa\n");
+    printf ("Opcao: ");
+    scanf ("%d",&o);
+}
+
+void insere (Controle *c)
+{
+    
+}
